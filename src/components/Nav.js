f0db76ce75023e9e24453351565fed09e41a8a0a
kimcoder <kimcoder@me.com>
2021-12-29 14:52:05 +0900

    docs: remove legacy blogs

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import Link from 'next/link';
-import logoSrc from '../images/logo.svg';
 import { siteConfig } from 'siteConfig';
 import { Search } from './Search';
 

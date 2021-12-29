a8113d402a7e2af06487681112775805d6716ce4
kimcoder <kimcoder@me.com>
2021-12-29 14:20:06 +0900

    docs: remove unused pages

--- src/siteConfig.js
+++ src/siteConfig.js
@@ -1,6 +1,5 @@
 // List of projects/orgs using your project for the users page.
 export const siteConfig = {
-  editUrl: 'https://github.com/tannerlinsley/react-query/edit/master/docs/src/pages',
   copyright: `Copyright © ${new Date().getFullYear()} kimcoder. All Rights Reserved.`,
   repoUrl: 'https://github.com/kimcoder',
   linkedIn: 'https://www.linkedin.com/in/daehyun-kim-b9a26716b/',

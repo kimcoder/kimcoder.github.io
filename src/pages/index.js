16aa630726012700ad3614f085aa5d338596ddd9
kimcoder <kimcoder@me.com>
2022-01-06 12:53:11 +0900

    fix: index.js remove getStaticProps

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,14 +4,14 @@ const Home = () => {
   return <></>;
 };
 
-export async function getStaticProps(context) {
-  return {
-    redirect: {
-      destination: '/overview',
-      permanent: true,
-    },
-  };
-}
+// export async function getStaticProps(context) {
+//   return {
+//     redirect: {
+//       destination: '/overview',
+//       permanent: true,
+//     },
+//   };
+// }
 
 export default Home;
 

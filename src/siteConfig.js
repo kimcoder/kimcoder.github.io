e3852a2389b89f618645f73187a3c5726f37b5df
kimcoder <kimcoder@me.com>
2022-12-13 20:53:20 +0900

    fix: update algolia api key

--- src/siteConfig.js
+++ src/siteConfig.js
@@ -5,8 +5,8 @@ export const siteConfig = {
   linkedIn: 'https://www.linkedin.com/in/daehyun-kim-b9a26716b/',
   facebook: 'https://www.facebook.com/kimcoder',
   algolia: {
-    appId: 'J1CRGFCJXS',
-    apiKey: 'e0f73b9dc9c2a90e5e47acfeca2aefb2',
+    appId: 'P33LW97X5Y',
+    apiKey: '15564d9e286c0d71c2766dd08c933d40',
     indexName: 'kimcoder.io', // algoliaOptions: {
     //   facetFilters: ['version:VERSION'],
     // },

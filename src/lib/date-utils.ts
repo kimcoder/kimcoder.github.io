4dd74affe9088a5997e747a07d288ff29b2e9507
kimcoder <kimcoder@me.com>
2023-01-24 18:20:19 +0900

    feat: ResumeSection 생성 및 자기소개 추가

--- src/lib/date-utils.ts
+++ src/lib/date-utils.ts
@@ -1,3 +1,9 @@
-import { format } from 'date-fns';
+import { format, compareAsc, differenceInYears } from 'date-fns';
 
 export const getDateString = (date: Date, formatStr: 'yyyy-MM' | 'yyyy.MM') => format(date, formatStr);
+
+export const sortDateAsc = (a: Date, b: Date) => compareAsc(a, b);
+
+export const diffYear = (a: Date, b: Date) => differenceInYears(a, b);
+
+export const diffYearFromNow = (target: Date) => Math.abs(diffYear(target, new Date()));
--- src/lib/resume/helpers.ts
+++ src/lib/resume/helpers.ts
@@ -1,4 +1,4 @@
-import { getDateString } from 'lib/date-utils';
+import { diffYearFromNow, getDateString, sortDateAsc } from 'lib/date-utils';
 import { CAREER } from 'lib/resume/career';
 
 export const enrichPeriod = (item: typeof CAREER[0]) => {
@@ -6,4 +6,11 @@ export const enrichPeriod = (item: typeof CAREER[0]) => {
   const endAtStr = item.endAt ? transformDate(item.beginAt) : '재직중';
 
   return { ...item, period: `${transformDate(item.beginAt)} ~ ${endAtStr}` };
+};
+
+export const getCareerDuration = (list: typeof CAREER) => {
+  const FIRST_YEAR = CAREER.map((item) => new Date(item.beginAt)).sort(sortDateAsc)[0];
+  const ADDITIONAL_YEAR = 1;
+
+  return diffYearFromNow(FIRST_YEAR) + ADDITIONAL_YEAR;
 };

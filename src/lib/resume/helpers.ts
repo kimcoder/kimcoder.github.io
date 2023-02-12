7dceab9bfa120859ee94d255fa37514d05f746b3
kimcoder <kimcoder@me.com>
2023-02-12 19:04:32 +0900

    feat: 이력서 UI 정리, 그외 경력 데이터 추가

--- src/lib/resume/helpers.ts
+++ src/lib/resume/helpers.ts
@@ -1,15 +1,18 @@
 import { diffYearFromNow, getDateString, sortDateAsc } from 'lib/date-utils';
 import { CAREER } from 'lib/resume/career';
 
-export const enrichPeriod = (item: typeof CAREER[0]) => {
-  const transformDate = (dateStr: typeof item['beginAt']) => getDateString(new Date(item.beginAt), 'yyyy.MM');
-  const endAtStr = item.endAt ? transformDate(item.beginAt) : '재직중';
+export const getPeriod = (beginAt: string, endAt?: string) => {
+  const transformDate = (dateStr: string) => getDateString(new Date(dateStr), 'yyyy.MM');
+  const endAtStr = endAt ? transformDate(endAt) : '재직중';
 
-  return { ...item, period: `${transformDate(item.beginAt)} ~ ${endAtStr}` };
+  return `${transformDate(beginAt)} ~ ${endAtStr}`;
 };
 
 export const getCareerDuration = (list: typeof CAREER) => {
-  const FIRST_YEAR = CAREER.map((item) => new Date(item.beginAt)).sort(sortDateAsc)[0];
+  const FIRST_YEAR = CAREER.map(({ team }) => team)
+    .flat()
+    .map((item) => new Date(item.beginAt))
+    .sort(sortDateAsc)[0];
   const ADDITIONAL_YEAR = 1;
 
   return diffYearFromNow(FIRST_YEAR) + ADDITIONAL_YEAR;
